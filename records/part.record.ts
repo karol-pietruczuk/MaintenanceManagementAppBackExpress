566cb8c7429937a4254d3967301ea7accfd7133f
Karol Pietruczuk <karol.pietruczuk@gmail.com>
2022-07-05 19:38:34 +0100

    Added PartRecord.

---
+++ records/part.record.ts
@@ -0,0 +1,39 @@
+import {ValidationError} from "../utils/error";
+import {PartEntity, PartUnit} from "../types";
+
+interface NewPartEntity extends Omit<PartEntity, 'id'> {
+    id?: string;
+}
+
+export class PartRecord implements NewPartEntity {
+
+    public id: string;
+    public name: string;
+    public amount: number = 0;
+    public unit: PartUnit = PartUnit.Quantity;
+    public storagePlace: string;
+    public createTime: Date;
+    public lastChangeTime: Date;
+
+    constructor(obj: NewPartEntity) {
+        if (!obj.name || obj.name.length > 50) {
+            throw new ValidationError('Nazwa części nie może być pusta, ani przekraczać 50 znaków.');
+        }
+
+        if (obj.amount > 999999.99 || obj.amount < 0) {
+            throw new ValidationError('Ilość części nie może być mniejsza od 0 lub większa od 999999.99.');
+        }
+
+        if (obj.storagePlace.length > 30) {
+            throw new ValidationError('Nazwa miejsca przechowywania części nie może być większe od 30.');
+        }
+
+        this.name = obj.name;
+        this.amount = obj.amount;
+        this.unit = obj.unit;
+        this.storagePlace = obj.storagePlace;
+    }
+
+
+
+}
--- types/part/part.entity.ts
+++ types/part/part.entity.ts
@@ -1,7 +1,13 @@
+export enum PartUnit{
+    Quantity = 'Quantity',
+    Meters = 'Meters',
+}
+
 export interface PartEntity {
     id: string;
     name: string;
     amount: number;
+    unit: PartUnit;
     storagePlace: string;
     createTime: Date;
     lastChangeTime: Date;
