dbfb4a40999b43fc2671bc4866ab65a379aa3a13
Karol Pietruczuk <karol.pietruczuk@gmail.com>
2022-07-05 01:50:11 +0100

    Added error handling.

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import 'express-async-errors';
 import {taskRouter} from "./Routers/task.router";
 import {partRouter} from "./Routers/part.router";
+import {handleError} from "./utils/error";
 
 const app = express();
 
@@ -10,6 +11,8 @@ app.use(express.json());
 app.use('/task', taskRouter);
 app.use('/part', partRouter);
 app.use('/order', partRouter);
+
+app.use(handleError);
 
 app.listen(3001, '0.0.0.0', () => {
     console.log('Listening on http://localhost:3001');
