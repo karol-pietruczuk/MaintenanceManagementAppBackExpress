7c4bc95fb4ecb8cb602e0cd0745794dcfcedde30
Karol Pietruczuk <karol.pietruczuk@gmail.com>
2022-07-07 01:49:18 +0200

    Added cors.

--- index.ts
+++ index.ts
@@ -1,11 +1,17 @@
 import express from 'express';
 import 'express-async-errors';
+import cors from "cors";
 import {taskRouter} from "./routers/task.router";
 import {partRouter} from "./routers/part.router";
 import {handleError} from "./utils/error";
 import {orderRouter} from "./routers/order.router";
+import {config} from "./config/config";
 
 const app = express();
+
+app.use(cors({
+    origin: config.corsOrigin,
+}));
 
 app.use(express.json());
 
